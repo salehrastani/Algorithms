925c394ec1e290c8cd51e139819df4eaa45d107d
Saleh Rastani <salehrastani@gmail.com>
2015-05-27 17:33:39 -0700

    add merge method that sorts the sliced arrays

--- merge_sort.rb
+++ merge_sort.rb
@@ -4,16 +4,24 @@ def merge_sort(array)
   if array.length <= 1
     array
   else
-    mid_point = (array/2).floor
-    left = mergesort(array[0...mid_point])
-    right = merge_sort(array[midpoint..-1])
+    mid_point = (array.length/2).floor
+    left = merge_sort(array[0...mid_point])
+    right = merge_sort(array[mid_point..-1])
     merge(left, right)
   end
 end
 
 def merge(left, right)
-
+  if left.empty?
+    right
+  elsif right.empty?
+    left
+  elsif left[0] > right[0]
+    [right[0]] + merge(right[1..-1], left)
+  else
+    [left[0]] + merge(left[1..-1], right)
+  end
 end
 
 
-merge_sort(array)
+p merge_sort(array)
